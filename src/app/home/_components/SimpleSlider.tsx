d6f43aa095ccda85a30c89aefb16fec3d24e3be2
SujanButang <sujanbutang@gmail.com>
2024-03-18 21:25:55 +0545

    feat: carousel

---
+++ src/app/home/_components/SimpleSlider.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function SimpleSlider() {
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  React.useEffect(() => {
+    console.log(current);
+  }, [current]);
+
+  const cardArray = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="flex w-full h-full items-center gap-10 flex-col">
+      <Carousel setApi={setApi} className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      <div className="flex items-center justify-between">
+        {cardArray.map((card, idx) => (
+          <Card key={idx} className={`${idx+1===current?"border-blue-700":""}`}>
+            <CardContent >{card}</CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,26 @@
+'use client'
+
 import Footer from "@/components/Footer";
+
 import Navbar from "@/components/Navbar";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {SimpleSlider} from "./_components/SimpleSlider";
+import { Card, CardContent } from "@/components/ui/card";
+import { useEffect, useState } from "react";
+import { CarouselApi } from "@/components/ui/carousel";
 
 const page = () => {
+
+ 
+
+
+  
   return (
-    <div className="w-full h-[100vh] flex flex-col justify-between">
-      <Navbar />
-      <Footer />
+    <div className="w-full h-[100vh] flex flex-col justify-center bg-white items-center gap-10">
+      <SimpleSlider />
+
+      
     </div>
   );
 };
