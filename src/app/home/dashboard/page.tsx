e2895e444396dc776f3cd7454d793fbbd3713033
SujanButang <sujanbutang@gmail.com>
2024-03-18 21:31:54 +0545

    feat: offset image

--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>this is home dashbiard</div>;
+  return <div className="w-full h-[100vh] flex flex-col justify-center bg-white items-center gap-10">
+    <div className="relative h-52 w-52 border-blue-800 border-2">
+      <img src="https://www.shutterstock.com/image-photo/rice-paddy-fields-dang-valley-260nw-441005785.jpg" alt="" className="w-full h-full object-cover absolute left-[-20px] top-[-20px]"/>
+    </div>
+  </div>;
 };
 
 export default page;
